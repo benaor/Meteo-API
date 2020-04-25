ab7c531ed7d0c60050582712592cc8dd46f2765f
benaor <benjamingirard25@gmail.com>
2020-04-26 00:33:29 +0200

    redirection to Chantrans if city not found

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-
-    let url = 'https://api.openweathermap.org/data/2.5/weather?q=' + ville + '&appid=910c86fa056451e51d511b3c005894cc&units=metric';
-
 let ville = 'chantrans';
 let villeHTML = document.getElementById('ville');
 villeHTML.textContent = ville;
 
 function recoverTemp() {
+
+    let url = 'https://api.openweathermap.org/data/2.5/weather?q=' + ville + '&appid=910c86fa056451e51d511b3c005894cc&units=metric';
+
 
     //Created a query Ajax
     let requete = new XMLHttpRequest();
@@ -23,16 +23,19 @@ function recoverTemp() {
                 let temperature = reponse.main.temp;
                 document.getElementById('temperature_label').textContent = temperature;
 
-            } else {
+            } else if (requete.status === 404) {
 
                 let reponse = requete.response;
                 let error = reponse.message;
                 alert(error);
-               
+                ville = 'chantrans';
+                villeHTML.textContent = ville;
+                return ville;
 
             }
 
         }
     }
+
 }
 setInterval(recoverTemp, 3000)
