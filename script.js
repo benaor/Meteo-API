3d231c1eaed2f6cb466ccd4f58568767c850b81d
benaor <benjamingirard25@gmail.com>
2020-04-26 00:31:51 +0200

    set interval for refresh

--- script.js
+++ script.js
@@ -1,36 +1,38 @@
 
-let url = 'https://api.openweathermap.org/data/2.5/weather?q=' + ville + '&appid=910c86fa056451e51d511b3c005894cc&units=metric';
+    let url = 'https://api.openweathermap.org/data/2.5/weather?q=' + ville + '&appid=910c86fa056451e51d511b3c005894cc&units=metric';
 
 let ville = 'chantrans';
 let villeHTML = document.getElementById('ville');
 villeHTML.textContent = ville;
 
+function recoverTemp() {
 
+    //Created a query Ajax
+    let requete = new XMLHttpRequest();
+    requete.open('GET', url);
+    requete.responseType = 'json';
+    requete.send();
 
-//Created a query Ajax
-let requete = new XMLHttpRequest();
-requete.open('GET', url);
-requete.responseType = 'json';
-requete.send();
+    requete.onload = function () {
 
-requete.onload = function () {
+        if (requete.readyState === XMLHttpRequest.DONE) {
 
-    if (requete.readyState === XMLHttpRequest.DONE) {
+            if (requete.status === 200) {
 
-        if (requete.status === 200) {
+                let reponse = requete.response
+                let temperature = reponse.main.temp;
+                document.getElementById('temperature_label').textContent = temperature;
 
-            let reponse = requete.response
-            let temperature = reponse.main.temp;
-            document.getElementById('temperature_label').textContent = temperature;
+            } else {
 
-        } else {
+                let reponse = requete.response;
+                let error = reponse.message;
+                alert(error);
+               
 
-            let reponse = requete.response;
-            let error = reponse.message;
-            alert(error);
-
+            }
 
         }
-
     }
 }
+setInterval(recoverTemp, 3000)
